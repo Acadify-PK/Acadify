cac056304ea1e99a47513c186ad20abcda3ee891
pzhafeez@gmail.com <pzhafeez@gmail.com>
2026-05-01 21:53:54 +0500

    Add route to retrieve full course details by ID

--- server/routes/course.routes.js
+++ server/routes/course.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createCourse, getAllCourses, getCourseById } from '../controllers/course.controller';
+import { createCourse, getAllCourses, getCourseById, getFullCourse } from '../controllers/course.controller';
 
 const router = express.Router();
 
 router.post("/", createCourse);
 router.get("/", getAllCourses);
 router.get("/:id", getCourseById);
+router.get("/full/:id", getFullCourse);
 
 export default router;
